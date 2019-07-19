32d0cad3744682d0d1834c8065de64ab95e59749
vinodaparthipan <50619660+vinodaparthipan@users.noreply.github.com>
2019-07-19 20:12:03 +0530

    Create mini.py

---
+++ mini.py
@@ -0,0 +1,8 @@
+s1,s2=input().split()
+cost_diff=abs(len(s1)-len(s2))
+for i in range(len(s1)):
+    if len(s2)==1 and s2[i] in s1:
+        break
+    if s1[i] != s2[i]:
+        cost_diff+=1
+print(cost_diff)
