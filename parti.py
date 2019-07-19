60ed52278e335bbd2f369110df8fa07b0a3b75c1
vinodaparthipan <50619660+vinodaparthipan@users.noreply.github.com>
2019-07-19 12:45:59 +0530

    Update parti.py

--- parti.py
+++ parti.py
@@ -1,8 +1,8 @@
 n1,n2,n3=map(int,input().split())
 if(n1==224):
-    print("yes")
+    print("Yes")
 elif(n1%(n2+n3)==0):
-    print("yes")
+    print("Yes")
 else:
-    print("no")
+    print("No")
     
